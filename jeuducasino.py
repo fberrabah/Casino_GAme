9f670cbad03f21b210ec62d908130bcead8c95d2
faridzweb <farid.berrabah@gmail.com>
2019-11-15 12:31:07 +0100

    add visual start game

--- jeuducasino.py
+++ jeuducasino.py
@@ -2,5 +2,9 @@ from math import ceil #add ceil for win with same color
 from random import randrange  #add randrange for random number
 import sys #for quit program with sys.exit()
 
+print("---------------------------------")
+print("Bienvenue au jeu de la roulette.") #visual information game 
+print("---------------------------------")
+
 money = 50 #player wallet
 while int(money) > 0 : # firt loop when money as > at 0
