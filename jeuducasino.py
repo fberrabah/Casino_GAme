e6c37f8cf4af37afa3d8a4ff5f346ec4270e05c8
faridzweb <farid.berrabah@gmail.com>
2019-11-15 12:41:54 +0100

    add choise number for game

--- jeuducasino.py
+++ jeuducasino.py
@@ -9,6 +9,11 @@ print("---------------------------------")
 money = 50 #player wallet
 while int(money) > 0 : # firt loop when money as > at 0
         if money > 0 : 
-        bet=int(input("Veuiller entrer votre mise : "))     
-        if bet < 1 or bet > money :         
-            bet=int(input("Veuillez entrer une mise possible : ")) #if answer not possible 
+            bet=int(input("Veuillez entrer votre mise : "))     
+            if bet < 1 or bet > money :         
+                bet=int(input("Veuillez entrer une mise possible : ")) #if answer not possible 
+        if bet <= money :
+            choice=int(input("choisisser votre numero entre 1 et 50 : ")) #add input with choice for number
+            if choice < 1 or choice > 50 : #if choise is not in range 1 at 50
+                choice=int(input("Attention, il faut choisir un numero entre 1 et 50 : "))
+        
